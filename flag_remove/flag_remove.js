09dd354a2b74197c05e2fa63ecc3397c075f0d99
Kees Wiegel <developer@iwantwin.nl>
2018-10-23 22:55:19 +0200

    script services reference

--- flag_remove/flag_remove.js
+++ flag_remove/flag_remove.js
@@ -87,7 +87,7 @@ javascript:/**UPDATED BY: iwantwin, twscripts@iwantwin.nl at 20-04-2016. Origina
     (function () {
       var b = "undefined" != typeof window.main ? window.main.document : document, a = b.createElement( "script" );
       a.type = "text/javascript"; 
-      a.src = "https://www.iwantwin.nl/scripts/tribalwars/iwantwinscriptsservices.js?ts=" + (new Date).getTime() + Math.round( 1E3 * Math.random() );
+      a.src = "https://raw.githubusercontent.com/iwantwin/tribalwars-scripts/master/iwantwin_script_services/iwantwin_scripts_services.js?ts=" + (new Date).getTime() + Math.round( 1E3 * Math.random() );
       b.getElementsByTagName( "head" )[0].appendChild( a );
       void 0
     })();
